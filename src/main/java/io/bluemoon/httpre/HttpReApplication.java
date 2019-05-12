201c86fffdab3e19060d6cf8cdf247496a0528c4
liquidjoo <liquidjoo@gmail.com>
2019-05-12 12:43:34 +0900

    project init

---
+++ src/main/java/io/bluemoon/httpre/HttpReApplication.java
@@ -0,0 +1,13 @@
+package io.bluemoon.httpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/bluemoon/httpre/HttpReApplicationTests.java
@@ -0,0 +1,16 @@
+package io.bluemoon.httpre;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpReApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
